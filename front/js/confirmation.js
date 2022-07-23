1d97f212545329280dce54726d87e85b63eb43ff
Brahima <brahimac95@gmail.com>
2022-07-23 16:25:42 +0200

    Affichage du numéro de commande

---
+++ front/js/confirmation.js
@@ -0,0 +1,5 @@
+//Affichage du numero de commande en recuperant l'id dans l'url
+let orderId = document.getElementById("orderId");
+
+let urlId = new URL(window.location.href).searchParams.get("orderId");
+orderId.textContent = urlId;
